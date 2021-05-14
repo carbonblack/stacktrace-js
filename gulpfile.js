3ba89a72a2e9ccebac80d4ff48ead4bf64b16d5c
Spencer Nowak <snowak@vmware.com>
2021-05-14 15:27:49 -0400

    Initial update to 2.0.2 upstream. DOES NOT consume forked GPS by hash, IS NOT built

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,7 @@
 var browserify = require('browserify');
 var concat = require('gulp-concat');
-var coveralls = require('gulp-coveralls');
 var del = require('del');
 var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-var karma = require('karma');
-var path = require('path');
 var rename = require('gulp-rename');
 var runSequence = require('run-sequence');
 var sourcemaps = require('gulp-sourcemaps');
@@ -20,22 +16,6 @@ var polyfills = [
 ];
 var sources = 'stacktrace.js';
 
-gulp.task('lint', function() {
-    return gulp.src(sources)
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'))
-        .pipe(jshint.reporter('fail'));
-});
-
-gulp.task('test', function(done) {
-    var server = new karma.Server({
-        configFile: __dirname + '/karma.conf.js',
-        singleRun: true
-    }, done);
-    server.start();
-});
-
-
 gulp.task('dist', function() {
     browserify({
         entries: sources,
@@ -45,13 +25,13 @@ gulp.task('dist', function() {
         .pipe(vinylSourceStream(sources))
         .pipe(gulp.dest('dist'))
         .pipe(vinylBuffer())
-        .pipe(rename({extname: '.min.js'}))
-        .pipe(uglify())
         .pipe(sourcemaps.init({loadMaps: true}))
+            .pipe(uglify())
+            .pipe(rename({extname: '.min.js'}))
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('dist'));
 
-    return browserify({
+    browserify({
         entries: polyfills.concat(sources),
         debug: true,
         standalone: 'StackTrace'
@@ -60,41 +40,15 @@ gulp.task('dist', function() {
         .pipe(vinylBuffer())
         .pipe(concat(sources.replace('.js', '-with-promises-and-json-polyfills.js')))
         .pipe(gulp.dest('dist'))
-        .pipe(uglify())
-        .pipe(rename({extname: '.min.js'}))
         .pipe(sourcemaps.init({loadMaps: true}))
+            .pipe(uglify())
+            .pipe(rename({extname: '.min.js'}))
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('test-pr', gulp.series('dist', function(done) {
-    new karma.Server({
-        configFile: __dirname + '/karma.conf.js',
-        browsers: ['Firefox', 'Chrome_Travis'],
-        singleRun: true
-    }, done).start();
-}));
+gulp.task('clean', del.bind(null, ['build', 'dist']));
 
-gulp.task('test-ci', gulp.series('dist', function(done) {
-    var server = new karma.Server({
-        configFile: __dirname + '/karma.conf.ci.js',
-        singleRun: true
-    }, done);
-    server.start();
-}));
-
-gulp.task('clean', del.bind(null, ['build', 'coverage', 'dist']));
-
-gulp.task('pr', gulp.series('lint', 'test-pr', function() {
-    gulp.src('./coverage/**/lcov.info')
-        .pipe(coveralls());
-}));
-
-gulp.task('ci', gulp.series('lint', 'test-ci', function() {
-    gulp.src('./coverage/**/lcov.info')
-        .pipe(coveralls());
-}));
-
-gulp.task('default', gulp.series('clean', function(cb) {
-    runSequence('lint', 'dist', 'test', cb);
-}));
+gulp.task('default', ['clean'], function(cb) {
+    runSequence('dist', 'test', cb);
+});
